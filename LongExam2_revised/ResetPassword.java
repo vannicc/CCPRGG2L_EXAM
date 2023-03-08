cb9d9950e4e5ac65bbd98cfead0a0c7266bacebc
Jerico Lim <111488202+vannicc@users.noreply.github.com>
2023-03-08 18:50:14 +0800

    Update ResetPassword.java

--- LongExam2_revised/ResetPassword.java
+++ LongExam2_revised/ResetPassword.java
@@ -7,8 +7,6 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.SystemColor;
@@ -64,83 +62,58 @@ public class ResetPassword {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panelFB = new JPanel();
-		panelFB.setLayout(null);
-		panelFB.setBackground(Color.WHITE);
-		panelFB.setBounds(10, 10, 1246, 663);
-		frame.getContentPane().add(panelFB);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 10, 1246, 663);
+		frame.getContentPane().add(panel);
 		
-		JPanel panelMain = new JPanel();
-		panelMain.setLayout(null);
-		panelMain.setBorder(new LineBorder(new Color(192, 192, 192)));
-		panelMain.setBackground(new Color(238, 238, 238));
-		panelMain.setBounds(0, 57, 1246, 464);
-		panelFB.add(panelMain);
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBorder(new LineBorder(new Color(192, 192, 192)));
+		panel_1.setBackground(new Color(238, 238, 238));
+		panel_1.setBounds(0, 57, 1246, 464);
+		panel.add(panel_1);
 		
-		JPanel panel_1Reset = new JPanel();
-		panel_1Reset.setLayout(null);
-		panel_1Reset.setBorder(new LineBorder(new Color(192, 192, 192)));
-		panel_1Reset.setBackground(Color.WHITE);
-		panel_1Reset.setBounds(325, 50, 590, 373);
-		panelMain.add(panel_1Reset);
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBorder(new LineBorder(new Color(192, 192, 192)));
+		panel_2.setBackground(Color.WHITE);
+		panel_2.setBounds(325, 50, 590, 373);
+		panel_1.add(panel_2);
 		
-		JPanel panel_2Reset = new JPanel();
-		panel_2Reset.setLayout(null);
-		panel_2Reset.setBorder(new LineBorder(new Color(192, 192, 192)));
-		panel_2Reset.setBackground(Color.WHITE);
-		panel_2Reset.setBounds(0, 0, 590, 58);
-		panel_1Reset.add(panel_2Reset);
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBorder(new LineBorder(new Color(192, 192, 192)));
+		panel_3.setBackground(Color.WHITE);
+		panel_3.setBounds(0, 0, 590, 58);
+		panel_2.add(panel_3);
 		
 		JLabel lblReset = new JLabel("Reset your password");
 		lblReset.setFont(new Font("Segoe UI", Font.BOLD, 17));
 		lblReset.setBounds(20, 10, 467, 38);
-		panel_2Reset.add(lblReset);
+		panel_3.add(lblReset);
 		
-		JPanel panel_3Reset = new JPanel();
-		panel_3Reset.setLayout(null);
-		panel_3Reset.setBorder(new LineBorder(new Color(192, 192, 192)));
-		panel_3Reset.setBackground(Color.WHITE);
-		panel_3Reset.setBounds(0, 289, 590, 84);
-		panel_1Reset.add(panel_3Reset);
+		JPanel panel_3_1 = new JPanel();
+		panel_3_1.setLayout(null);
+		panel_3_1.setBorder(new LineBorder(new Color(192, 192, 192)));
+		panel_3_1.setBackground(Color.WHITE);
+		panel_3_1.setBounds(0, 289, 590, 84);
+		panel_2.add(panel_3_1);
 		
 		JButton btnContinue = new JButton("Continue");
-		btnContinue.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				boolean SignUpSuccess = false;
-				String pass = pfNewPass.getText().toString();
-				
-				User inputChecker = new User("", ""); 
-					boolean isValidPassword = inputChecker.checkPassword(pass);
-	
-				
-				if ( pfNewPass.getText().equals("")) {
-					JOptionPane.showMessageDialog(null, "Please enter your password!");
-				} else if (pfNewPass.getText().toString().equals(pass)) {
-					if (isValidPassword) {
-						JOptionPane.showMessageDialog(null, "Sign up success!\nClick 'OK' to return to Log In.");
-						frame.dispose();
-						EnrolmentForm enroll = new EnrolmentForm();
-							enroll.main(null);
-					} else {
-						JOptionPane.showMessageDialog(null, "Please enter a valid password!\n\nPassword must be:\na. 8 characters\nb. With upper and lower case letters\nc. Contains at least one special character");
-					}
-				} else {
-					JOptionPane.showMessageDialog(null, "Invalid Password! Please try again.");
-				}
-			}
-		});
 		btnContinue.setOpaque(true);
 		btnContinue.setForeground(Color.WHITE);
 		btnContinue.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		btnContinue.setBorderPainted(false);
 		btnContinue.setBackground(new Color(0, 128, 255));
 		btnContinue.setBounds(460, 20, 109, 41);
-		panel_3Reset.add(btnContinue);
+		panel_3_1.add(btnContinue);
 		
 		JButton btnNotYou = new JButton("Not you?");
 		btnNotYou.addMouseListener(new MouseAdapter() {
 			@Override
+      // When mouse is clicked, returns to the Log In Page
 			public void mouseClicked(MouseEvent e) {
 				LoginPage login = new LoginPage();
 				frame.dispose();
@@ -153,23 +126,23 @@ public class ResetPassword {
 		btnNotYou.setBorderPainted(false);
 		btnNotYou.setBackground(SystemColor.controlHighlight);
 		btnNotYou.setBounds(339, 20, 109, 41);
-		panel_3Reset.add(btnNotYou);
+		panel_3_1.add(btnNotYou);
 		
 		JLabel lblQuestion1 = new JLabel("Create a new password that is at least 6 characters long.");
 		lblQuestion1.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		lblQuestion1.setBounds(21, 68, 466, 35);
-		panel_1Reset.add(lblQuestion1);
+		panel_2.add(lblQuestion1);
 		
 		JLabel lblQuestion2 = new JLabel("A strong password is combination of letters, numbers, and");
 		lblQuestion2.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		lblQuestion2.setBounds(21, 93, 466, 23);
-		panel_1Reset.add(lblQuestion2);
+		panel_2.add(lblQuestion2);
 		
 		JLabel lblGmail2 = new JLabel("mikoclark19@gmail.com");
 		lblGmail2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGmail2.setFont(new Font("Segoe UI", Font.PLAIN, 17));
 		lblGmail2.setBounds(355, 201, 212, 35);
-		panel_1Reset.add(lblGmail2);
+		panel_2.add(lblGmail2);
 		
 		txtFbUser = new JTextField();
 		txtFbUser.setText("Facebook User");
@@ -179,25 +152,26 @@ public class ResetPassword {
 		txtFbUser.setColumns(10);
 		txtFbUser.setBorder(null);
 		txtFbUser.setBounds(409, 233, 107, 23);
-		panel_1Reset.add(txtFbUser);
+		panel_2.add(txtFbUser);
 		
-		JLabel lblFbIcon = new JLabel("");
-		Image img = new ImageIcon(this.getClass().getResource("HNSPOST.png")).getImage();
+		JLabel lblFbIcon = new JLabel(""); 
+    // Created the Image and Image Icon class to add image to the JLabel
+		Image img = new ImageIcon(this.getClass().getResource("profile.jpg")).getImage();
 		lblFbIcon.setBounds(409, 94, 91, 99);
-		panel_1Reset.add(lblFbIcon);
+		panel_2.add(lblFbIcon);
 		lblFbIcon.setIcon(new ImageIcon(img));
 		
-		JLabel lblQuestion3 = new JLabel("punctuation marks.");
-		lblQuestion3.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblQuestion3.setBounds(21, 113, 466, 23);
-		panel_1Reset.add(lblQuestion3);
+		JLabel lblPunctuationMarks = new JLabel("punctuation marks.");
+		lblPunctuationMarks.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		lblPunctuationMarks.setBounds(21, 113, 466, 23);
+		panel_2.add(lblPunctuationMarks);
 		
 		JPanel panelNewPass = new JPanel();
 		panelNewPass.setLayout(null);
 		panelNewPass.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(192, 192, 192)));
 		panelNewPass.setBackground(Color.WHITE);
 		panelNewPass.setBounds(21, 174, 217, 42);
-		panel_1Reset.add(panelNewPass);
+		panel_2.add(panelNewPass);
 		
 		pfNewPass = new JPasswordField();
 		pfNewPass.setBounds(10, 5, 197, 32);
@@ -209,14 +183,17 @@ public class ResetPassword {
 		pfNewPass.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		pfNewPass.addFocusListener(new FocusAdapter() {
 			@Override
+      // When focus is gained, sets password field to empty and then start to hide the inputted value
 			public void focusGained(FocusEvent e) {
 			if (pfNewPass.getText().equals("Password")) {
+				pfNewPass.setEchoChar('●');
 				pfNewPass.setText("");
 			} else {
 				pfNewPass.selectAll();
 			}
 			}
 			@Override
+      // DEFAULT: When focus is lost, reverts to the default password field text
 			public void focusLost(FocusEvent e) {
 				if (pfNewPass.getText().equals("")) {
 					pfNewPass.setText("Password");
@@ -224,21 +201,23 @@ public class ResetPassword {
 			}
 		});
 		
-		JButton btnShow = new JButton("Show");
-		btnShow.addMouseListener(new MouseAdapter() {
+		JButton btnNewButton = new JButton("Show");
+		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
+      // When button is pressed, shows the input password
 			public void mousePressed(MouseEvent e) {
 				pfNewPass.setEchoChar((char)0);
 			}
 			@Override
+      // When mouse is released from the button, hides the input password again
 			public void mouseReleased(MouseEvent e) {
 				pfNewPass.setEchoChar('●');
 			}
 		});
-		btnShow.setBackground(new Color(234, 234, 234));
-		btnShow.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnShow.setBounds(248, 174, 71, 42);
-		panel_1Reset.add(btnShow);
+		btnNewButton.setBackground(new Color(234, 234, 234));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton.setBounds(248, 174, 71, 42);
+		panel_2.add(btnNewButton);
 		
 		
 		
@@ -246,11 +225,12 @@ public class ResetPassword {
 		lblFacebook.setForeground(new Color(0, 128, 255));
 		lblFacebook.setFont(new Font("Tahoma", Font.BOLD, 30));
 		lblFacebook.setBounds(10, 0, 427, 57);
-		panelFB.add(lblFacebook);
+		panel.add(lblFacebook);
 		
 		JButton btnLogIn = new JButton("Log in");
 		btnLogIn.addMouseListener(new MouseAdapter() {
 			@Override
+      // When button is clicked, returns to the Log In Page
 			public void mouseClicked(MouseEvent e) {
 				LoginPage login = new LoginPage();
 				frame.dispose();
@@ -265,14 +245,14 @@ public class ResetPassword {
 		btnLogIn.setFont(new Font("Segoe UI", Font.BOLD, 19));
 		btnLogIn.setBackground(new Color(0, 128, 255));
 		btnLogIn.setBounds(1124, 10, 101, 37);
-		panelFB.add(btnLogIn);
+		panel.add(btnLogIn);
 		
 		JPanel panel_4 = new JPanel();
 		panel_4.setLayout(null);
 		panel_4.setBorder(new LineBorder(new Color(238, 238, 238)));
 		panel_4.setBackground(Color.WHITE);
 		panel_4.setBounds(0, 575, 1246, 88);
-		panelFB.add(panel_4);
+		panel.add(panel_4);
 		
 		JLabel lblOther1 = new JLabel("Services   Voting Information Center   Privacy Policy   Privacy Center   Groups   About   Create Ad   Create Page   Developers   Careers   Cookies   Ad choices");
 		lblOther1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -306,7 +286,7 @@ public class ResetPassword {
 		lblLanguages.setForeground(new Color(114, 114, 114));
 		lblLanguages.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		lblLanguages.setBounds(318, 546, 598, 23);
-		panelFB.add(lblLanguages);
+		panel.add(lblLanguages);
 		
 		JLabel lblNewLabel = new JLabel("+");
 		lblNewLabel.setOpaque(true);
@@ -315,6 +295,6 @@ public class ResetPassword {
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNewLabel.setBackground(new Color(238, 238, 238));
 		lblNewLabel.setBounds(915, 547, 18, 18);
-		panelFB.add(lblNewLabel);
+		panel.add(lblNewLabel);
 	}
 }
